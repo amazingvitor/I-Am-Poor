61675c5a7d3636dcbc27ab8e4ff03e1df2b0642c
Vitor Hugo S <vitor.h8@icloud.com>
2019-12-01 14:55:58 -0300

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Vitor Hugo on 12/1/19.
+//  Copyright © 2019 Vitor Hugo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
